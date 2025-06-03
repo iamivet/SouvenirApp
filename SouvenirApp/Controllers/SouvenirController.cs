625691f56c7a75f5f3caa2ad1b6717ae6c0d06ce
iamivet <ivetdimi25@abv.bg>
2025-06-03 17:20:05 +0300

    Start with SouvenirController

---
+++ SouvenirApp/Controllers/SouvenirController.cs
@@ -0,0 +1,24 @@
+﻿using SouvenirApp.Data;
+using SouvenirApp.Data.Models;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace SouvenirApp.Controllers
+{
+    public class SouvenirController
+    {
+        private SouvenirAppContext dbContext = new SouvenirAppContext();
+
+        public List<Souvenir> GetAllSOuvenirs()
+           => dbContext.Souvenirs.Include("SouvenirTypes").ToList();
+
+        public void Create(Souvenir souvenir)
+        {
+            dbContext.Souvenirs.Add(souvenir);
+            dbContext.SaveChanges();
+
+        }
+
+    
+    }
+}
