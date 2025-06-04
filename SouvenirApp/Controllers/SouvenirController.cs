c549cf88cd320674c2b3a31e4320cd2f88061e63
Teodor168 <teodor16iliev@gmail.com>
2025-06-04 08:47:26 +0300

    Change SouvenirController

--- SouvenirApp/Controllers/SouvenirController.cs
+++ SouvenirApp/Controllers/SouvenirController.cs
@@ -19,6 +19,35 @@ namespace SouvenirApp.Controllers
 
         }
 
+      public Souvenir Get(int Id)
+        {
+            Souvenir findedSouvenir = dbContext.Souvenirs.Find(Id);
+
+            if(findedSouvenir != null)
+            {
+               dbContext.Entry(findedSouvenir).Reference(s => s.Type).Load();  
+            }
+
+           return findedSouvenir;
+        }
+
+
+        public void Update(int id,Souvenir souvenir)
+        {
+            Souvenir findedSouvenir = dbContext.Souvenirs.Find(id);
+
+            if (findedSouvenir == null)
+                return;
+
+            findedSouvenir.Name = souvenir.Name;
+            findedSouvenir.Description = souvenir.Description;
+            findedSouvenir.Price = souvenir.Price;
+            findedSouvenir.TypeId = souvenir.TypeId;
+
+
+            dbContext.SaveChanges();
+
+        }
     
     }
 }
